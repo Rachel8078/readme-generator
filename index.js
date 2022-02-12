4d7e0cc1fd05fef7b6bed9ca614e2f539881532d
Rachel <rachelmmarriott@gmail.com>
2022-02-11 20:03:16 -0700

    add inquirer

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const inquirer = require('inquirer');
 const generateReadMe = require('./src/readme-template.js');
-const profileDataArgs = process.argv.slice(2);
 
-const [projectName, description, installation, usage, contributing, license, name, githubName, email] = profileDataArgs;
+//const readMeContent = generateReadMe(projectName, description, installation, usage, contributing, license, name, githubName, email);
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));

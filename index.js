e8182be16c67572f8cd999fd5e41ef8d47f5918c
Rachel <rachelmmarriott@gmail.com>
2022-02-11 20:33:49 -0700

    add questions

--- index.js
+++ index.js
@@ -3,12 +3,55 @@ const generateReadMe = require('./src/readme-template.js');
 
 //const readMeContent = generateReadMe(projectName, description, installation, usage, contributing, license, name, githubName, email);
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
-        }
-    ])
-    .then(answers => console.log(answers));
+            message: 'Hi, what is your name?'
+        },
+        {
+            type: 'input',
+            name: 'githubName',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        },
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project.'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please include installation instructions for your project.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please include usage information for your project.'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Please include contributing guidelines for your project.'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please choose which license your project is covered under.',
+            choices: ['MIT', 'GNU GPLv3']
+        }, 
+    ]);
+};
+
+promptUser().then(answers => console.log(answers));
